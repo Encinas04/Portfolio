3de98b54c520ba18e6a9a1d8303bbb51c764f74f
hector <hencinas23@gmail.com>
2026-01-28 12:14:57 +0100

    .

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+
+function Header() {
+
+    return (
+        <header>
+            <h1>Portfolio de Hector</h1>
+        </header>
+    );
+
+}
+export default Header;
---
+++ src/data/projects.js
@@ -0,0 +1,33 @@
+
+export const projects = [
+    {
+        id: 1,
+        title: "To-Do List Pro",
+        description: "Gestor de tareas con persistencia de datos (LocalStorage). Permite crear, borrar y marcar tareas.",
+        image: "https://placehold.co/600x400/222/fff?text=To-Do+List", // Imagen temporal generada autom.
+        tech: ["React", "useState", "LocalStorage"],
+        github: "#",
+        demo: "#",
+        status: "En desarrollo" // <--- Ojo a esto, nos servirá para el diseño
+    },
+    {
+        id: 2,
+        title: "Movie Finder",
+        description: "Buscador de películas consumiendo la API de OMDB. Muestra pósters y detalles en tiempo real.",
+        image: "https://placehold.co/600x400/444/fff?text=Buscador+Cine",
+        tech: ["React", "API", "useEffect"],
+        github: "#",
+        demo: "#",
+        status: "Planeado"
+    },
+    {
+        id: 3,
+        title: "Calculadora Retro",
+        description: "Calculadora funcional con diseño Grid y lógica matemática completa.",
+        image: "https://placehold.co/600x400/666/fff?text=Calculadora",
+        tech: ["React", "CSS Grid", "Lógica"],
+        github: "#",
+        demo: "#",
+        status: "Planeado"
+    }
+];
